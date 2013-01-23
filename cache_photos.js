e507f3400a4567ab807d85db2d71e83908c35541
David Stolarsky <david.stolarsky@gmail.com>
2013-01-23 13:19:07 -0500

    latlng order

--- cache_photos.js
+++ cache_photos.js
@@ -1,42 +1,39 @@
-/*var ExifImage = require('exif').ExifImage;
-
-try {
-    var path = process.argv[2];
-    console.log("Opening " + path);
-    new ExifImage({ image : path }, function (error, image) {
-        if (error)
-            console.log('Error: '+error.message);
-        else
-            console.log(image.gps); // Do something with your data!
-    });
-} catch (error) {
-    console.log('Error: ' + error);
-}*/
-
-
 var im = require('imagemagick');
-var path = process.argv[2];
-console.log("Opening " + path);
-im.readMetadata(path, function(err, metadata){
-  if (err) throw err;
-  
-  // gpsLatitude: '39/1, 4419/100, 0/1',
-  // gpsLatitudeRef: 'N',
-  // gpsLongitude: '75/1, 3267/100, 0/1',
-  // gpsLongitudeRef: 'W',
-  
-  var location = [metadata.exif.gpsLongitude, metadata.exif.gpsLatitude].map(function(str){
-    var parts = str.split(',');
-    var degrees = parseInt(parts[0].split('/')[0]);
-    var minutesFraction = parts[1].split('/').map(function(n){return parseInt(n); });
-    var minutes = minutesFraction[0] / minutesFraction[1];
-    degrees += minutes / 60;
-    return degrees;
-  });
-  if(metadata.exif.gpsLatitudeRef == 'S')
-    location[1] *= -1;
-  if(metadata.exif.gpsLongitudeRef == 'W')
-    location[0] *= -1;
 
-  console.log(location);
-})
+function pathToGeoPhoto(path, callback)
+{
+  console.log("Opening " + path);
+  im.readMetadata(path, function(err, metadata){
+    if (err) throw err;
+    
+    // gpsLatitude: '39/1, 4419/100, 0/1',
+    // gpsLatitudeRef: 'N',
+    // gpsLongitude: '75/1, 3267/100, 0/1',
+    // gpsLongitudeRef: 'W',
+    
+    var location = [metadata.exif.gpsLatitude, metadata.exif.gpsLongitude].map(function(str){
+      var parts = str.split(',');
+      var degrees = parseInt(parts[0].split('/')[0]);
+      var minutesFraction = parts[1].split('/').map(function(n){return parseInt(n); });
+      var minutes = minutesFraction[0] / minutesFraction[1];
+      degrees += minutes / 60;
+      return degrees;
+    });
+    if(metadata.exif.gpsLatitudeRef == 'S')
+      location[0] *= -1;
+    if(metadata.exif.gpsLongitudeRef == 'W')
+      location[1] *= -1;
+  
+    callback({
+      'path' : path,
+      'location' : location,
+      'time' : metadata.exif.dateTime
+    });
+  });
+}
+
+var arg = process.argv[2];
+
+pathToGeoPhoto(arg, function(photo){
+  console.log(photo);
+});
