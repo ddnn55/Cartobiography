366bf5c195cea81e55bf8d30b0c8091458cf69ee
David Stolarsky <david.stolarsky@gmail.com>
2013-01-23 14:50:38 -0500

    dump geophoto json array

--- cache_photos.js
+++ cache_photos.js
@@ -1,6 +1,10 @@
-var im = require('imagemagick');
+var im = require('imagemagick'),
+    fs = require('fs'),
+    path = require('path'),
+    async = require('async');
 
-function pathToGeoPhoto(path, callback)
+var geoPhotos = [];
+function loadGeoPhotoWorker(path, done)
 {
   console.log("Opening " + path);
   im.readMetadata(path, function(err, metadata){
@@ -11,6 +15,11 @@ function pathToGeoPhoto(path, callback)
     // gpsLongitude: '75/1, 3267/100, 0/1',
     // gpsLongitudeRef: 'W',
     
+    if(metadata.exif.gpsLatitude === undefined) {
+      done();
+      return;
+    }
+
     var location = [metadata.exif.gpsLatitude, metadata.exif.gpsLongitude].map(function(str){
       var parts = str.split(',');
       var degrees = parseInt(parts[0].split('/')[0]);
@@ -24,16 +33,33 @@ function pathToGeoPhoto(path, callback)
     if(metadata.exif.gpsLongitudeRef == 'W')
       location[1] *= -1;
   
-    callback({
+    geoPhotos.push({
       'path' : path,
       'location' : location,
       'time' : metadata.exif.dateTime
     });
+
+    done();
   });
 }
 
-var arg = process.argv[2];
+var dir = process.argv[2];
 
-pathToGeoPhoto(arg, function(photo){
-  console.log(photo);
+fs.readdir(dir, function(err, files) {
+  jpgs = files.filter(function(file) { return /jpg/i.test(file); });
+  jpgs = jpgs.map(function(file) { return path.join(dir, file); });
+  console.log('jpgs: ', jpgs);
+  
+  var q = async.queue(loadGeoPhotoWorker, 32);
+  q.drain = function() {
+    console.log('All photos have been processed:');
+    console.log(geoPhotos);
+  };
+
+  jpgs.forEach(function(path) {
+    q.push(path, function(err) {
+      console.log('Finished processing ' + path);
+    });
+  });
+
 });
